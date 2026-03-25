e1394f6b755fdebb228d0318dbffa1a74f97a4c4
hambakim <bnmil8274@gmail.com>
2026-03-25 10:19:29 +0900

    GitHub 원격 저장소 초기화

--- src/mul.c
+++
@@ -1,6 +0,0 @@
-#include "calc.h"
-
-int mul(int a, int b) {
-    return a * b;
-}
-
--- src/print_result.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "calc.h"
-
-void print_result(const char *label, int result) {
-    printf("%s °á°ú: %d\n", label, result);
-}
-
